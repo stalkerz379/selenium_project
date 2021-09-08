6a039c9e8e10e96f2c87f5bbe4eddc6eb8385842
= <yaruk_den@mail.ru>
2021-09-08 16:35:25 +0300

    added tests for the login page, based on its own class

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 link = 'http://selenium1py.pythonanywhere.com/'
@@ -16,7 +17,27 @@ def test_guest_can_go_to_login_page(browser):
 
 
 def test_guest_should_see_login_link(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_login_url_should_contain_login_word(browser):
+    link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+
+def test_login_form_should_present(browser):
+    link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+def test_should_be_login_form(browser):
+    link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
