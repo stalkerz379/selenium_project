61008aaf5d23c95591d2a45415752d723414e913
= <yaruk_den@mail.ru>
2021-09-09 14:01:51 +0300

    added new Selectors, fixed old problems

--- pages/locators.py
+++ pages/locators.py
@@ -13,5 +13,7 @@ class LoginPageLocators():
 class ProductPageLocators():
     ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, '.btn.btn-lg.btn-add-to-basket')
     PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
-    SUCCESS_MESSAGE_BLOCK = (By.CSS_SELECTOR, '.alert.alert-success:nth-child(1)')
-    DEFERRED_BENEFIT_ORDER = (By.CSS_SELECTOR, '.alert.alert-success:nth-child(2)')
+    SUCCESS_MESSAGE_BLOCK = (By.CSS_SELECTOR, '.alert.alert-success:nth-child(1) strong')
+    DEFERRED_BENEFIT_ORDER = (By.CSS_SELECTOR, '.alert.alert-success:nth-child(2) strong')
+    PRODUCT_PRICE = (By.CSS_SELECTOR, '.product_main p[class="price_color"]')
+    BLOCK_WITH_PRICE = (By.CSS_SELECTOR, '.alertinner p strong')
