fc426de04075a475c0ca3e66b5845ea52a57bb0b
= <yaruk_den@mail.ru>
2021-09-09 21:58:59 +0300

    added tests for check if element not present on page, present on page, and disappears from page

--- test_product_page.py
+++ test_product_page.py
@@ -24,3 +24,25 @@ def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.product_page()
+
+
+@pytest.mark.xfail(reason='temporary a bug')
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart_button_press()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail(reason='not fixed, bug')
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_cart_button_press()
+    page.should_disappear()
