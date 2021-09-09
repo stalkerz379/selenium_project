5620eb1bcf64d9e0ed4885824c7ba62039d2a9eb
= <yaruk_den@mail.ru>
2021-09-09 14:02:43 +0300

    added new methods for checking price and product name, fixed selector issues

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,21 +9,27 @@ class ProductPage(BasePage):
         self.solve_quiz_and_get_code()
         self.should_be_message_added_to_cart()
         self.should_be_message_cart_satisfies_order()
-
+        self.price_should_be_equal_to()
 
     def add_to_cart_button_press(self):
         add_to_cart = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
         add_to_cart.click()
 
+    def get_product_price_and_name(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        return product_name, product_price
+
     def should_be_message_added_to_cart(self):
-        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
-        message_block = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_BLOCK)
-        product_name_text = product_name.text       # get the name of the product
-        message_block_text = message_block.text     # get the text of the cart block
-        print(product_name_text)
-        assert product_name_text in message_block_text, f'{product_name} not in {message_block_text}'
+        product_name, product_price = self.get_product_price_and_name()
+        message_block = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE_BLOCK).text
+        assert product_name == message_block, f'{product_name} not equal to {message_block}'
 
     def should_be_message_cart_satisfies_order(self):
-        message_satisfy_order = self.browser.find_element(*ProductPageLocators.DEFERRED_BENEFIT_ORDER)
-        message_satisfy_order_text = message_satisfy_order.text
-        assert 'Deferred benefit offer' in message_satisfy_order_text, f'Deferred benefit order not in {message_satisfy_order_text}'
+        message_satisfy_order = self.browser.find_element(*ProductPageLocators.DEFERRED_BENEFIT_ORDER).text
+        assert 'Deferred benefit offer' == message_satisfy_order, f'Deferred benefit order not equal to {message_satisfy_order}'
+
+    def price_should_be_equal_to(self):
+        product_name, product_price = self.get_product_price_and_name()
+        block_with_product_price = self.browser.find_element(*ProductPageLocators.BLOCK_WITH_PRICE).text
+        assert product_price == block_with_product_price, f'{product_price} is not equal to {block_with_product_price}'
