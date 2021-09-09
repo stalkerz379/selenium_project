a6a91571c4626dfb64ed61ba04b2b7b38e979238
= <yaruk_den@mail.ru>
2021-09-09 09:25:16 +0300

    edited syntax mistakes

--- test_main_page.py
+++ test_main_page.py
@@ -10,16 +10,18 @@ def go_to_login_page(browser):
     login_link.click()
 
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-
-
 def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_login_url_should_contain_login_word(browser):
