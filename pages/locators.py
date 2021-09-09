62372d68ce96f24152a3c1acc0423c1d813e8052
= <yaruk_den@mail.ru>
2021-09-09 09:24:36 +0300

    created variables/selectors for the product page

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, '.btn.btn-lg.btn-add-to-basket')
+    PRODUCT_NAME = (By.CSS_SELECTOR, '.product_main h1')
+    SUCCESS_MESSAGE_BLOCK = (By.CSS_SELECTOR, '.alert.alert-success:nth-child(1)')
+    DEFERRED_BENEFIT_ORDER = (By.CSS_SELECTOR, '.alert.alert-success:nth-child(2)')
