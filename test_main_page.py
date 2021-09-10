0b11ecce4d330648d180e223e78f1c0c92d32882
= <yaruk_den@mail.ru>
2021-09-10 19:23:33 +0300

    added a class for login page test, grouped them into the class

--- test_main_page.py
+++ test_main_page.py
@@ -6,15 +6,18 @@ import pytest
 link = 'http://selenium1py.pythonanywhere.com/'
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_go_to_login_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        login_link = browser.find_element_by_css_selector("#login_link")
+        login_link.click()
 
-
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page(browser):
