a3280d549c3c623756fa45700b61280f37f4a0b9
= <yaruk_den@mail.ru>
2021-09-10 08:59:43 +0300

    added new methods to check the presence of the login link on any page

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,6 +4,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
 
 
 class BasePage():
@@ -50,3 +51,16 @@ class BasePage():
         except TimeoutException:
             return False
         return True
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK_INVALID)
+        login_link.click()
+        alert = self.browser.switch_to.alert
+        alert.accept()
+
+    def go_to_login_page_valid(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
