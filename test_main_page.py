066b170eedf0b2aba8cbfbae82ce5ebc6a597980
= <yaruk_den@mail.ru>
2021-09-10 10:53:53 +0300

    added new methods to check the basket if it is empty

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
-
+from .pages.basket_page import BasketPage
+import pytest
 
 link = 'http://selenium1py.pythonanywhere.com/'
 
@@ -19,7 +20,7 @@ def test_guest_should_see_login_link(browser):
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+    page.go_to_login_page_valid()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
@@ -43,3 +44,51 @@ def test_should_be_login_form(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_register_form()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    page.basket_should_not_have_items()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = 'https://selenium1py.pythonanywhere.com/catalogue/'
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_product_page()
+    page.go_to_basket()
+    page.basket_should_not_have_items()
+    page.text_basket_page_is_empty()
+
+
+@pytest.mark.skip(reason='Not used now, need to be updated')
+def test_empty_text(browser):
+    languages = {"ar": "سلة التسوق فارغة",
+                 "ca": "La seva cistella està buida.",
+                 "cs": "Váš košík je prázdný.",
+                 "da": "Din indkøbskurv er tom.",
+                 "de": "Ihr Warenkorb ist leer.",
+                 "en": "Your basket is empty.",
+                 "el": "Το καλάθι σας είναι άδειο.",
+                 "es": "Tu carrito esta vacío.",
+                 "fi": "Korisi on tyhjä",
+                 "fr": "Votre panier est vide.",
+                 "it": "Il tuo carrello è vuoto.",
+                 "ko": "장바구니가 비었습니다.",
+                 "nl": "Je winkelmand is leeg",
+                 "pl": "Twój koszyk jest pusty.",
+                 "pt": "O carrinho está vazio.",
+                 "pt-br": "Sua cesta está vazia.",
+                 "ro": "Cosul tau este gol.",
+                 "ru": "Ваша корзина пуста",
+                 "sk": "Váš košík je prázdny",
+                 "uk": "Ваш кошик пустий.",
+                 "zh-cn": "Your basket is empty."}
+    link = 'https://selenium1py.pythonanywhere.com/catalogue/'
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_product_page()
+    page.go_to_basket()
+    page.check_empty_basket_text(languages)
