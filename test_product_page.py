d32ac38b3af4afd6b72fd68f5a8074cba6ce5156
= <yaruk_den@mail.ru>
2021-09-11 14:07:09 +0300

    added new class and 2 test for this class with setuped data for tests, tests are for authorized user

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,7 @@
 from .pages.product_page import ProductPage
 import pytest
+import time
+from .pages.login_page import LoginPage
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 
@@ -60,3 +62,26 @@ def test_guest_can_go_to_login_page_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page_valid()
+
+
+@pytest.mark.login
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + '@mail.com'
+        password = str(time.time()) + 'Ab.'
+        link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+    def test_guest_cant_see_success_message(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_guest_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, link)
+        page.open()
+        page.product_page()
